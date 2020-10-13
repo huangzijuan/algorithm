434982327a8b974d667f2bc9ff152cafc8b6289a
ORION-20200818S\Orion <huangzijuan@ainirobot.com>
2020-10-13 17:25:44 +0800

    20201013

--- testJava/src/tree/BaseTree.java
+++ testJava/src/tree/BaseTree.java
@@ -1,5 +1,9 @@
 package tree;
 
+import util.CommonUtils;
+
+import java.util.*;
+
 public class BaseTree {
     public static class TreeNode {
         public int value;
@@ -11,16 +15,126 @@ public class BaseTree {
         }
     }
 
+    public TreeNode buildTree(TreeNode root, int[] numbers, int index) {
+        if (index >= numbers.length) {
+            return null;
+        }
+        root = new TreeNode(numbers[index]);
+        root.left = buildTree(null, numbers, 2*index + 1);
+        root.right = buildTree(null, numbers, 2*index + 2);
+        return root;
+    }
+
+
     public static void main(String[] args) {
         BaseTree baseTree = new BaseTree();
-
+        int[] nums = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8};
+        TreeNode root = baseTree.buildTree(null, nums, 0);
+        List<Integer> result = baseTree.levelOrderTraversal1(root);
+        //List<Integer> result = baseTree.preorderTraversal2(root);
+        //List<Integer> result = baseTree.levelOrderTraversal(root);
+        CommonUtils.printArray3(result);
     }
 
 
     /**
-     * 中序遍历二叉树
+     * 前序遍历二叉树（递归法）
      */
-    public void midTraversal() {
-
+    public List<Integer> preorderTraversal1(TreeNode root) {
+        ArrayList<Integer> resultList = new ArrayList<>();
+        preTraversal(root, resultList);
+        return resultList;
     }
+
+    public void preTraversal(TreeNode root, ArrayList<Integer> resultList) {
+        if (root == null) return;
+        resultList.add(root.value);
+        preTraversal(root.left, resultList);
+        preTraversal(root.right, resultList);
+    }
+
+    /**
+     * 前序遍历二叉树（非递归法）
+     */
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        ArrayList<Integer> resultList = new ArrayList<>();
+        Stack<TreeNode> treeNodeStack = new Stack<>();
+        treeNodeStack.push(root);
+        while (!treeNodeStack.empty()) {
+            TreeNode treeNode = treeNodeStack.pop();
+            resultList.add(treeNode.value);
+            if (treeNode.right != null) {
+                treeNodeStack.push(treeNode.right);
+            }
+            if (treeNode.left != null) {
+                treeNodeStack.push(treeNode.left);
+            }
+        }
+        return resultList;
+    }
+
+
+    /**
+     * 广度优先搜索遍历二叉树
+     */
+    public List<Integer> levelOrderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> tempQueue = new ArrayDeque<>();
+        tempQueue.add(root);
+        while (!tempQueue.isEmpty()) {
+            TreeNode treeNode = tempQueue.poll();
+            result.add(treeNode.value);
+            if (treeNode.left != null) {
+                tempQueue.offer(treeNode.left);
+            }
+            if (treeNode.right != null) {
+                tempQueue.offer(treeNode.right);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 之字形打印二叉树
+     */
+    public List<Integer> levelOrderTraversal1(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> to = new Stack<>();
+        to.push(root);
+        levelOrderTraversal11(1, to, result);
+        return result;
+    }
+    public void levelOrderTraversal11(int level, Stack<TreeNode> from, List<Integer> result) {
+        if (from.isEmpty()) {
+            return;
+        }
+
+        Stack<TreeNode> to = new Stack<>();
+
+        while (!from.isEmpty()) {
+            TreeNode treeNode = from.pop();
+            result.add(treeNode.value);
+            System.out.print(" " + treeNode.value);
+            if (level % 2 == 0) {
+                if (treeNode.left != null) {
+                    to.push(treeNode.left);
+                }
+
+                if (treeNode.right != null) {
+                    to.push(treeNode.right);
+                }
+            } else {
+                if (treeNode.right != null) {
+                    to.push(treeNode.right);
+                }
+
+                if (treeNode.left != null) {
+                    to.push(treeNode.left);
+                }
+            }
+        }
+        System.out.println();
+        levelOrderTraversal11(++level, to, result);
+    }
+
 }
