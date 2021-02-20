00494df6f0e2be9feb57741dd06c5de3c87853d4
ORION-20200818S\Orion <huangzijuan@ainirobot.com>
2021-02-20 20:55:00 +0800

    '20210220'

--- testJava/src/geek/week1.java
+++ testJava/src/geek/week1.java
@@ -1,4 +1,94 @@
 package geek;
 
+import java.util.*;
+
 public class week1 {
+
+    /**
+     * 盛最多水的容器
+     * 方法一：暴力解法
+     */
+    public int maxArea1(int[] height) {
+        int maxArea = 0;
+        for(int i = 0; i < height.length - 1; i++) {
+            for (int j = i + 1; j < height.length; j++) {
+                int area = (j - i) * Math.min(height[i], height[j]);
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+        return maxArea;
+    }
+
+    // 方法二：双指针
+    public int maxArea2(int[] height) {
+        int maxArea = 0;
+        for (int i = 0, j = height.length - 1; i < j;) {
+            int area = (j - i) * (height[i] > height[j] ? height[j--] : height[i++]);
+            maxArea = Math.max(maxArea, area);
+        }
+        return maxArea;
+    }
+
+    /**
+     * 移动零
+     * 方法一：双指针，用j记录第一个非0元素的位置
+     */
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i != j) {
+                    nums[j] = nums[i];
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+
+    /**
+     * 爬楼梯
+     */
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+
+    /**
+     * 三数之和
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> resultSet = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int target = -nums[i];
+            int j = i + 1;
+            int k = nums.length - 1;
+            while (j < k) {
+                if (nums[j] + nums[k] < target) {
+                    j++;
+                } else if (nums[j] + nums[k] > target) {
+                    k--;
+                } else {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(nums[k]);
+                    if (!resultSet.contains(temp)) {
+                        resultSet.add(temp);
+                        result.add(temp);
+                    }
+                    j++;
+                    k--;
+                }
+            }
+        }
+        return result;
+    }
 }
