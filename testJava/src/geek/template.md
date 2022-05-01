1c9796bda479966e1b6e9d9f27dc63498ba7b53a
ORION-20200818S\Orion <huangzijuan@ainirobot.com>
2022-05-01 11:07:42 +0800

    20220501

--- testJava/src/geek/template.md
+++ testJava/src/geek/template.md
@@ -1,10 +1,11 @@
 package geek;
 
-## 点点
+## 知识点记忆
 1. 在一般的电脑中,int占用4字节,32比特,数据范围为-2147483648~2147483647[-2^31~2^31-1]
+2. 常用的区间一般为左闭右开区间
 
+```
 public class template {
-
     // 递归代码模板
     public void recur(int level, int param) {
         
@@ -25,3 +26,4 @@ public class template {
         // restore current status 
     }
 }
+```
--- testJava/src/geek/week4.java
+++ testJava/src/geek/week4.java
@@ -1,4 +1,126 @@
 package geek;
 
+import tree.TreeNode;
+
+import java.util.*;
+
 public class week4 {
+
+    /**
+     * 102 层序遍历
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        Deque<TreeNode> deque = new ArrayDeque();
+        deque.offer(root);
+        while (deque.size() > 0) {
+            int size = deque.size();
+            List<Integer> tempList = new ArrayList<>();
+            while (size-- != 0) {
+                TreeNode top = deque.poll();
+                tempList.add(top.val);
+                if (top.left != null) {
+                    deque.offer(top.left);
+                }
+
+                if (top.right != null) {
+                    deque.offer(top.right);
+                }
+            }
+            result.add(tempList);
+        }
+        return result;
+    }
+
+    /**
+     * 433 最小基因变化
+     * 即寻找树的最短路径的问题
+     * 单向BFS法
+     */
+    public int minMutation(String start, String end, String[] bank) {
+        int count = 0;
+        Character[] dnaSeq = {'A', 'C', 'G', 'T'};
+        List<String> backList = new ArrayList<>(Arrays.asList(bank));
+        Deque<String> queue = new ArrayDeque();
+        queue.add(start);
+
+        if (!backList.contains(end)) {
+            return -1;
+        }
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                String head = queue.poll();
+                for (int i = 0; i < head.length(); i++) {
+                    for (int j = 0; j < dnaSeq.length; j++) {
+                        String newStr = head.substring(0, i) + dnaSeq[j] + head.substring(i+1);
+
+                        if (backList.contains(newStr)) {
+                            queue.offer(newStr);
+                            backList.remove(newStr);   // remove的作用相当于标记visited的作用
+                        }
+
+                        if (newStr.equals(end)) {
+                            return count;
+                        }
+                    }
+                }
+            }
+            count++;
+        }
+
+        return count;
+    }
+
+    /**
+     * 22 括号生成
+     */
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        parenthesisHelper(result, 0, 0, "", n);
+        return result;
+    }
+
+    private void parenthesisHelper(List<String> result, int leftNum, int rightNum, String curStr, int n) {
+        if (leftNum == n && rightNum == n) {
+            result.add(curStr);
+            return;
+        }
+
+        if (leftNum < n) {
+            parenthesisHelper(result, leftNum + 1, rightNum, curStr + "(", n);
+        }
+
+        if (leftNum > rightNum) {
+            parenthesisHelper(result, leftNum, rightNum + 1, curStr + ")", n);
+        }
+    }
+
+    /**
+     * 515 每个树行中找最大值
+     */
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Deque<TreeNode> deque = new ArrayDeque();
+        deque.offer(root);
+        while (deque.size() > 0) {
+            int size = deque.size();
+            int max = Integer.MIN_VALUE;
+            while (size-- != 0) {
+                TreeNode top = deque.poll();
+                max = Math.max(max, top.val);
+                if (top.left != null) {
+                    deque.offer(top.left);
+                }
+
+                if (top.right != null) {
+                    deque.offer(top.right);
+                }
+            }
+            result.add(max);
+        }
+        return result;
+    }
 }
